d0ebed2aa21e6a7497331c78e401d099e3e6dba0
econ-dev-avishka <avishka@econsulate.net>
2024-11-11 13:37:55 +0530

    Basic socket setup

---
+++ src/gateway/gatway.ts
@@ -0,0 +1,32 @@
+import { OnModuleInit } from '@nestjs/common';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway()
+export class MyGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('Connected');
+    });
+  }
+
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body: any) {
+    console.log(body);
+
+    const returnData = {
+      msg: 'Socket message received',
+      data: body,
+    };
+    this.server.emit('onMessage', returnData);
+  }
+}
